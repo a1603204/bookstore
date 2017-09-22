36ec290953fbac954e0b0122c45f079f381dee72
Fomina Nadezhda <a1603204@myy.haaga-helia.fi>
2017-09-22 12:38:32 +0300

    First commit

---
+++ src/main/java/fi/haagahelia/course/bookstore/web/BookController.java
@@ -0,0 +1,50 @@
+package fi.haagahelia.course.bookstore.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fi.haagahelia.course.bookstore.domain.Book;
+import fi.haagahelia.course.bookstore.domain.BookRepository;
+import fi.haagahelia.course.bookstore.domain.Book;
+
+@Controller
+public class BookController {
+	@Autowired
+	private BookRepository repository; 
+	
+	  @RequestMapping(value = "/save", method = RequestMethod.POST)
+	    public String save(Book book){
+	        repository.save(book);
+	        return "redirect:booklist";
+	    }   
+	   
+    @RequestMapping(value="/booklist")
+    public String bookList(Model model) {	
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
+  
+    @RequestMapping(value = "/add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+        return "addbooklist";
+    }    
+    
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.delete(bookId);
+        return "redirect:../booklist";
+    } 
+    
+    @RequestMapping(value = "/edit/{id}")
+    public String addBook(@PathVariable("id") Long bookId, Model model){
+    model.addAttribute("book", repository.findOne(bookId));
+    model.addAttribute("departments", repository.findAll());
+    return "editbooklist";
+    }
+    
+}
